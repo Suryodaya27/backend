0c581948f3d9e42b073132839ecdf3441fa64bf9
Suryodaya pandey <79349719+Suryodaya27@users.noreply.github.com>
2023-06-12 15:29:26 +0530

    userinfo updated

--- routes/userInfo.js
+++ routes/userInfo.js
@@ -16,6 +16,19 @@ router.get('/users/:userId/loans', verifyToken, async (req, res) => {
     }
 
     // Retrieve the user's issued loans and their statuses, including loan and bank details
+    const userinfo = await prisma.user.findUnique({
+      where: {
+        id: parseInt(userId),
+      },
+      select:{
+        id:true,
+        username:true,
+        email:true,
+
+      }
+    })
+
+
     const loans = await prisma.application.findMany({
       where: {
         userId: parseInt(userId),
@@ -32,7 +45,7 @@ router.get('/users/:userId/loans', verifyToken, async (req, res) => {
     });
 
     // Retrieve the user's DSA commission if user is DSA
-    const dsa = await prisma.dsa.findFirst({
+    const dsainfo = await prisma.dsa.findFirst({
       where: { dsaId: parseInt(userId) },
       select: { 
         totalCommission: true ,
@@ -42,13 +55,13 @@ router.get('/users/:userId/loans', verifyToken, async (req, res) => {
       },
     });
     
-    const commission = dsa ? dsa.totalCommission  : null;
-    const remainingCommision= dsa ? dsa.commissionRemaining : null;
-    const amountLoan = dsa ? dsa.amountLoan : null;
-    const loansIssued = dsa ? dsa.loansIssued : null;
+    // const commission = dsa ? dsa.totalCommission  : null;
+    // const remainingCommision= dsa ? dsa.commissionRemaining : null;
+    // const amountLoan = dsa ? dsa.amountLoan : null;
+    // const loansIssued = dsa ? dsa.loansIssued : null;
     
 
-    res.status(200).json({ loans, commission,remainingCommision,amountLoan, loansIssued});
+    res.status(200).json({ userinfo,loans, dsainfo});
   } catch (error) {
     console.error('Error retrieving issued loans:', error);
     res.status(500).json({ error: 'An error occurred while retrieving issued loans' });
